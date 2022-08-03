e8b0c6c091e137b4f213c5ab6243db8ef951dd12
dtoolz <donaldaudriel@gmail.com>
2022-08-03 19:01:54 +0100

    insert method added to core model.php

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -6,7 +6,18 @@ class Home extends Controller
 {
       function index()
       {
-        $user = $this->load_model('User');
+        //$user = $this->load_model('User');
+        $user = new user();
+
+        $arr['firstname'] = 'peter';
+        $arr['lastname'] = 'pan';
+        $arr['date'] = '2022-08-01 17:22:26';
+        $arr['user_id'] = 'rtryfkfmvkgkg';
+        $arr['gender'] = 'male';
+        $arr['school_id'] = 'eyrhhffnhdhd';
+        $arr['rank'] = 'student';
+        
+        $user->insert($arr);
         $data = $user->findAll();
          //$data = $user->where('firstname', 'sam');
          $this->view('home',['rows'=>$data]); //calling the Controller which it extends by using this keyword to pass params
--- private/core/model.php
+++ private/core/model.php
@@ -2,10 +2,13 @@
 
 class Model extends Database 
 {
-    protected $table = "users";
+    //protected $table = "users";
     function __construct()
     {
-        
+        if(!property_exists($this, 'table'))
+        {
+            $this->table = strtolower($this::class)."s";
+        }
     }
 
     public function where($column,$value) 
@@ -21,5 +24,33 @@ class Model extends Database
     {
        $query = "select * from $this->table ";
        return $this->query($query);
+    }
+
+    public function insert($data) 
+    {
+       $keys = array_keys($data);
+       $columns = implode(',', $keys);
+       $values = implode(',:', $keys);
+       $query = "insert into $this->table ($columns) values (:$values)";
+       return $this->query($query,$data);
+    }
+
+
+    public function update($id,$data) 
+    {
+       $column = addslashes($column);
+       $query = "select * from $this->table where $column = :value";
+       return $this->query($query,[
+         'value' => $value
+       ]);
+    }
+
+    public function delete($id) 
+    {
+       $column = addslashes($column);
+       $query = "select * from $this->table where $column = :value";
+       return $this->query($query,[
+         'value' => $value
+       ]);
     }
 }
